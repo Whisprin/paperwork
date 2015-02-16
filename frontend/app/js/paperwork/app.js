45bccac0fd03926d1bdd995f4d928362c4b80dc5
Whisprin <Whisprin@users.noreply.github.com>
2015-02-16 12:00:29 +0100

    Change absolute urls to relative paths
    - changed image urls and api base path
    - this enables the possibility to run the app from subfolders

--- frontend/app/js/paperwork/app.js
+++ frontend/app/js/paperwork/app.js
@@ -1,2 +1,2 @@
 angular.module('paperworkNotes', ['ngRoute', 'ngSanitize', 'ngAnimate', 'angularFileUpload', 'ab-base64'])
-.constant('paperworkApi', '/api/v1');
+.constant('paperworkApi', 'api/v1');
